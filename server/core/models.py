c3a6beb64131c4312d38e5b93773df7e58c61c39
Gabriel Lorenzo <gabriellorenzo@Gabriels-MacBook-Pro.local>
2026-09-17 20:12:05 -0400

    feat: add vehicle and vehicle owner models

--- server/core/models.py
+++ server/core/models.py
@@ -1,3 +1,15 @@
-from django.db import models
+from django.db import models 
 
-# Create your models here.
+
+class VehicleOwner(models.Model):
+    name = models.CharField(max_length=100)
+
+class Vehicle(models.Model):
+    car_number = models.CharField(max_length=3, unique=True)
+    license_plate_number = models.CharField(max_length=7, unique=True)
+    
+    owner = models.ForeignKey(
+        VehicleOwner,
+        on_delete=models.PROTECT,
+        related_name='vehicles'
+    )
